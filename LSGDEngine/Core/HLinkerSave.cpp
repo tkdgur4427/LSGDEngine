253920379cd526093df82533b083beeaf609230d
hsh6679 <hsh6679@bluehole.net>
2018-11-05 20:38:03 +0900

    continue to implement linker save

--- LSGDEngine/Core/HLinkerSave.cpp
+++ LSGDEngine/Core/HLinkerSave.cpp
@@ -1,10 +1,120 @@
 #include "HCorePCH.h"
 #include "HLinkerSave.h"
 
+// HObject
+#include "HObject.h"
+
 using namespace lsgd;
+using namespace reflect;
 
 HLinkerSave::HLinkerSave()
 	: SrcPackageToSave(nullptr)
 {
 
+}
+
+struct HTagExportObjectsRecursive : public HReflectionContext
+{
+	HTagExportObjectsRecursive(HPackage* InPackage)
+		: OutermostPackage(InPackage)
+	{}
+
+	virtual HReflectionContext& operator<<(class HObject*& Value) override
+	{
+		if (Value->Package == OutermostPackage)
+		{
+			// tag the current object as export object
+			Value->State.TagObjectFlags(EObjectFlags::TagExp);
+
+			// do the recursive tagging
+			Value->Serialize(*this);
+
+			// add tagged object
+			TaggedObjects.push_back(Value);
+		}
+
+		return *this;
+	}
+
+	typedef HObject* TaggedObjectType;
+
+	HArray<TaggedObjectType> TaggedObjects;
+	HPackage* OutermostPackage;
+};
+
+struct HTagNameRecursive : public HReflectionContext
+{
+	HTagNameRecursive(HArray<HName>& OutNameMap)
+		: NameMap(OutNameMap)
+	{}
+
+	virtual HReflectionContext& operator<<(class HName& Value) override
+	{
+		NameMap.push_back(Value);
+		return *this;
+	}
+
+	virtual HReflectionContext& operator<<(class HObject*& Value) override
+	{
+		if (Value->State.ObjectFlags & (EObjectFlags::TagExp | EObjectFlags::TagImp) != 0)
+		{
+			// only when object is tagged by import or export object, serialize HName
+			*this << Value->Name;
+
+			// serialize recursively
+			Value->Serialize(*this);
+		}
+
+		return *this;
+	}
+
+	HArray<HName>& NameMap;
+};
+
+bool HLinkerSave::SavePackage(HLinkerSaveContext& InContext)
+{
+	// setting attributes
+	LinkerRoot = InContext.OutermostPkg;
+
+	HArray<HObject*>& ObjectsToSave = InContext.ObjectsToSave;
+
+	// tag archive
+	HTagExportObjectsRecursive TagExportArchive(LinkerRoot);
+
+	// tag the HObjects to save as export recursively
+	for (HObject* Object : ObjectsToSave)
+	{
+		TagExportArchive << Object;
+	}
+
+	// tag import objects from export objects
+	for (HObject* Object : TagExportArchive.TaggedObjects)
+	{
+		// @todo - need to tag 'TagImp'
+		// note that do NOT tag import recursively
+	}
+
+	// create export map
+	for (HObject* Object : TagExportArchive.TaggedObjects)
+	{
+		int32 OutIndex = ExportMap.size();
+		ExportMap.push_back(HObjectExport());
+
+		ExportMap[OutIndex].OuterIndex = OutIndex + 1;
+		ExportMap[OutIndex].Object = Object;
+		ExportMap[OutIndex].ObjectName = Object->Name;
+	}
+
+	// create import map
+	// @todo
+
+	// add name to name-map by HTagNameRecursive
+	HTagNameRecursive SerializeNameArchive(NameMap);
+	for (HObjectExport& ObjectExport : ExportMap)
+	{
+		SerializeNameArchive << ObjectExport.Object;
+	}
+	
+	// initialize TOC
+	
 }
--- LSGDEngine/Core/HLinkerSave.h
+++ LSGDEngine/Core/HLinkerSave.h
@@ -9,10 +9,19 @@ namespace lsgd
 		** 2018.11.3
 			- single-threaded LinkerSave like exactly UE3 or UE4
 	*/
+
+	struct HLinkerSaveContext
+	{
+		class HPackage* OutermostPkg;
+		HArray<class HObject*> ObjectsToSave;
+	};
+
 	class HLinkerSave : public HLinker , public reflect::HReflectionContext
 	{
 	public:
 		HLinkerSave();
+
+		bool SavePackage(HLinkerSaveContext& InContext);
 
 		const HPackage* SrcPackageToSave;
 	};
