5b354194841931f392a877f43e336892bd4feda3
tkdgur4427 <tkdgur4427@gmail.com>
2019-10-04 16:27:41 +0900

    add the BattleServer

---
+++ LSGDEngine/BattleServer/HGameInstanceOverride.cpp
@@ -0,0 +1,42 @@
+#include "HBattleServerPCH.h"
+#include "HGameInstanceOverride.h"
+
+using namespace lsgd;
+
+IMPLEMENT_CLASS_TYPE1(HGameInstanceOverride, HGameInstance)
+
+IMPLEMENT_CLASS_TYPE1(HIocpDummy, HObject)
+
+void HGameInstanceOverride::Reflect()
+{
+
+}
+
+void HGameInstanceOverride::Initialize()
+{
+	HGameInstance::Initialize();
+
+	IpDriver = HObjectHandleUnique<HIpDriver>(AllocateHObject(HIpDriver::GetClassName()));
+	IpDriver.SetRoot();
+
+	IpDriver->Initialize();
+}
+
+void HGameInstanceOverride::Destroy()
+{
+	IpDriver->Destroy();
+
+	HGameInstance::Destroy();
+}
+
+void HGameInstanceOverride::Tick(float DeltaTime)
+{
+	HGameInstance::Tick(DeltaTime);
+
+	IpDriver->Tick(DeltaTime);
+}
+
+void HIocpDummy::Reflect()
+{
+	reflect::HTypeDatabase::GetSingleton()->AddClassField<HIocpDummy>("Data", &HIocpDummy::Data);
+}
